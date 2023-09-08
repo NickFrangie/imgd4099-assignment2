5cb7e79b7f4b333f3e6f10bb3079aa258cfd45a7
Glitch (dear-lead-close) <none>
2023-09-08 16:17:22 +0000

    ⚽♦️ Updated with Glitch

--- main.js
+++ main.js
@@ -2,28 +2,25 @@ import { default as seagulls } from './seagulls.js'
 import { default as Video    } from './video.js'
 import { default as Audio    } from './audio.js'
 
-const shader = `
-@group(0) @binding(0) var<uniform> frame: f32;
-@group(0) @binding(1) var<uniform> res:   vec2f;
-@group(0) @binding(2) var<uniform> audio: vec3f;
-@group(0) @binding(3) var<uniform> mouse: vec3f;
-@group(0) @binding(4) var backSampler:    sampler;
-@group(0) @binding(5) var backBuffer:     texture_2d<f32>;
-@group(0) @binding(6) var videoSampler:   sampler;
-@group(1) @binding(0) var videoBuffer:    texture_external;
+var shader = importText("wsgl.txt");
 
-@vertex 
-fn vs( @location(0) input : vec2f ) ->  @builtin(position) vec4f {
-  return vec4f( input, 0., 1.); 
+function importText(textFile) {
+    var rawFile = new XMLHttpRequest();
+    var allText = "";
+    rawFile.open("Get", textFile, false);
+    rawFile.onreadystatechange = function()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allText = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
 }
-
-@fragment 
-fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
-  let vid = textureSampleBaseClampToEdge( videoBuffer, videoSampler, pos.xy / res );
-  let fb  = textureSample( backBuffer, backSampler, pos.xy / res );
-  let out = vid * .05 + fb * .95;
-  return vec4f( out.r, 0.,0. , 1. );
-}`
 
 async function main() {
   let frame = 0
